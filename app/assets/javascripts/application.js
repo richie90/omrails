162ca0a4c8d4e201797dedafcaa6b3d7035d94e0
Richie Dan <richie.dan90@gmail.com>
2013-08-03 22:17:52 +1000

    Add Endless Scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.masonry.pkgd.min.js
+//= require jquery.masonry.min.js
 //= require bootstrap
 //= require_tree .
--- app/assets/javascripts/pins.js
+++
@@ -1,7 +0,0 @@
-jQuery(function() {
-  return $('#pins').imagesLoaded(function() {
-    return $('#pins').masonry({
-      itemSelector: ".box"
-    });
-  });
-});
